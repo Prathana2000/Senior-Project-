95e94d85e1c0902ffb4548b4483288c5ad4c38f4
Prathana <prathana.khuan2000@gmail.com>
2023-03-18 23:01:23 +0700

    update dynamic color type

--- src/components/ResultLabel.js
+++ src/components/ResultLabel.js
@@ -1,11 +1,32 @@
 import React from "react";
 
 export default function ResultLabel({ result }) {
-    let color = "#29ab49"
+  let color = "";
+  switch (result.CLASS_NAME) {
+    // ['MyMo', 'บัตรและสลากออมสิน', 'สินเชื่อ', 'อื่น ๆ', 'เงินฝาก']
+    case "MyMo":
+      color = "red";
+      break;
+    case "บัตรและสลากออมสิน":
+      color = "pink";
+      break;
+    case "สินเชื่อ":
+      color = "green";
+      break;
+    case "อื่น ๆ":
+      color = "lightgray";
+      break;
+    case "เงินฝาก":
+      color = "yellow";
+      break;
+    default:
+      color = "blue";
+      break;
+  }
 
   return (
     <div>
-      <div style={{color}}>{result.CLASS_NAME}</div>
+      <div style={{ color }}>{result.CLASS_NAME}</div>
       <div>{result.PROBABILITY}</div>
     </div>
   );
