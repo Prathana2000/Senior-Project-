6beddc4c261c6cf71c804b0b836b880b863b9c1a
Prathana <prathana.khuan2000@gmail.com>
2023-03-30 22:52:06 +0700

    create contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/About" element={<About />}/>
+      <Route path="/Contact" element={<Contact />}/>
     </Routes>
   </BrowserRouter>
   );
