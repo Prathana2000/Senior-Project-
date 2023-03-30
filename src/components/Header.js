c5d40cca76447b6a3637e9bfa39b2c85de72235d
Prathana <prathana.khuan2000@gmail.com>
2023-03-30 23:06:12 +0700

    create link to cantact

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,36 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { FiCode, FiMenu, FiX } from "react-icons/fi";
-import './Header.scss'
+import "./Header.scss";
 import { Link } from "react-router-dom";
 
 function Header() {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-    return (
-        <div className="header">
-            <div className="header-con">
-                <div className="element-con">
-                    <div className="logo-container">
-                    <Link to="/">HOME</Link>
-                    </div>
-                    <ul className= {click ? "menu active" : "menu"}>
-                        <li className="menu-link" onClick={closeMobileMenu}>
-                        <Link to="/About">ABOUT</Link>
-                        </li>
-                        <li className="menu-link" onClick={closeMobileMenu}>
-                            <a href="#">CONTACT</a>
-                        </li>
-                    </ul>
-                    <div className="mobile-menu" onClick={handleClick}>
-                        {click ?(
-                            <FiX/>
-                        ) : (
-                            <FiMenu/>
-                        )}
-
-                    </div>
-                </div>
-            </div> 
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <div className="header">
+      <div className="header-con">
+        <div className="element-con">
+          <div className="logo-container">
+            <Link to="/">HOME</Link>
+          </div>
+          <ul className={click ? "menu active" : "menu"}>
+            <li className="menu-link" onClick={closeMobileMenu}>
+              <Link to="/About">ABOUT</Link>
+            </li>
+            <li className="menu-link" onClick={closeMobileMenu}>
+              <Link to="/Contact">CONTACT</Link>
+            </li>
+          </ul>
+          <div className="mobile-menu" onClick={handleClick}>
+            {click ? <FiX /> : <FiMenu />}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
