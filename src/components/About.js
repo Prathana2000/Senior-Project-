d97ef10ebc691e5db054f15ba778aeeb272ba444
Prathana <prathana.khuan2000@gmail.com>
2023-05-04 02:18:14 +0700

    add photo for create About.js
    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./About.scss";
 
 function About() {
   return (
-    <div className="content-layout">
-      <p>About</p>
+    <div className="about-layout">
+      <div className="profile one">
+        <img src={require("../assets/img/About/jame.jpg")} />
+        <p>นายสุทธิพงศ์ กรรณิกากลาง</p>
+        <p>jame.kannikaklang@gmail.com</p>
+      </div>
+      <div className="profile two">
+        <img src={require("../assets/img/About/jame.jpg")} />
+        <p>นางสาวปรารถนา เขื่อนขวาวงศ์</p>
+        <p>prathana.khuan2000@gmail.com</p>
+      </div>
+      <div className="profile three">
+        <img src={require("../assets/img/About/khim.jpg")} />
+        <p>นางสาวณัฐนรี แดงสกุล</p>
+        <p>knatnareennr@gmail.com</p>
+      </div>
     </div>
   );
 }
