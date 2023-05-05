f02328a90cb9c66b95c663883c2e9426367e920e
Prathana <prathana.khuan2000@gmail.com>
2023-05-05 16:48:09 +0700

    contact edit style
    change style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Contact.scss";
+import { GrLocation } from "react-icons/gr";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
 
 function Contact() {
   return (
     <div className="contact-layout">
-      <h7 className="title-text">Bachelor of Science Program, Computer Science.</h7>
+      <div className="title-text">
+        <h7>Srinakharinwirot University</h7>
+        <br />
+        <h7>Bachelor of Science Program, Computer Science.</h7>
+      </div>
+
       <div className="contact-profile">
         <div className="profile one">
           <img src={require("../assets/img/About/jame.jpg")} />
@@ -30,10 +37,25 @@ function Contact() {
       </div>
       <div className="about">
         <div className="about-title">
-          <h3>Contact Us</h3>
+          <h4>Contact Us</h4>
         </div>
         <div className="about-text">
-
+          <h8 className="about-name">
+            <GrLocation />
+            LOCATION
+          </h8>
+          <p>
+            ชั้น 18 อาคาร 19 มหาวิทยาลัยศรีนครินทรวิโรฒ ประสานมิตร 114 สุขุมวิท
+            23 แขวงคลองเตยเหนือ เขตวัฒนา กรุงเทพฯ 10110
+          </p>
+          <h8 className="about-name">
+            <MdOutlineMarkEmailUnread />
+            EMAIL
+          </h8>
+          <p>
+            jame.kannikaklang@gmail.com, prathana.khuan2000@gmail.com,
+            knatnareennr@gmail.com
+          </p>
         </div>
       </div>
     </div>
